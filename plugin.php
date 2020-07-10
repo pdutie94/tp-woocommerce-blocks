0e14f1f2a80b0ed489f11317e8acc14e0a6b1016
pdutie94 <ptiencntt94@gmail.com>
2020-07-10 23:06:07 +0700

    Create plugin

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: TP Woocommerce Blocks
+ * Plugin URI: https://tienpham.xyz/tp-woocommerce-blocks/
+ * Description: Gutenberg blocks for Woocommerce
+ * Author: Tiến Phạm
+ * Author URI: https://tienpham.xyz/
+ */
